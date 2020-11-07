7bad294e493b96801984bfdeb84d44c7de1d6814
윤세영 <dbstpdud09@naver.com>
2020-11-08 00:24:59 +0900

    레시피 Start Page 구성

--- app/src/main/java/kr/co/bakeapplication/views/RecipeActivity.kt
+++ app/src/main/java/kr/co/bakeapplication/views/RecipeActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.bumptech.glide.Glide
 import kr.co.bakeapplication.R
 import kr.co.bakeapplication.data.Recipe
 import kr.co.bakeapplication.data.RecipePage
@@ -30,6 +32,12 @@ class RecipeActivity : AppCompatActivity() {
 
         val pageAdapter = ScreenSlidePagerAdapter(recipe.pages!!, supportFragmentManager)
         mBinding.layoutRecipeViewpager.adapter = pageAdapter
+
+        Glide.with(this).load(recipe.thumbNailUri).into(mBinding.imageviewRecipeThumbnailBackground)
+
+        mBinding.buttonRecipeStartrecipe.setOnClickListener {
+            mBinding.layoutRecipeStartpage.visibility = View.GONE
+        }
     }
 
     private inner class ScreenSlidePagerAdapter(private val recipePages: ArrayList<RecipePage>,fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
